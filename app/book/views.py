b76729d55a54aaaafa6ef3485bb96cc414ea499b
szapata1014 <szapata1@stevens.edu>
2016-11-30 12:52:53 -0500

    Added templates and bootsrap

--- app/book/views.py
+++ app/book/views.py
@@ -1,9 +1,10 @@
-
 from flask import render_template, Blueprint
+from app.models import Book
 
 book_blueprint = Blueprint('book', __name__, template_folder = 'templates')
 
 
 @book_blueprint.route('/')
 def index():
-	return render_template('index.html')
+	all_books = Book.query.all()
+	return render_template('books.html', books = all_books)
