0bc9cc92f72d2df9054250e2b96e9f9e2929626d
szapata1014 <szapata1@stevens.edu>
2016-12-01 13:09:08 -0500

    Added email change option

--- app/user/views.py
+++ app/user/views.py
@@ -168,3 +168,29 @@ def reset_with_token(token):
 def user_profile():
 	return render_template('user_profile.html')
 
+@user_blueprint.route('/email_change', methods=["GET", "POST"])
+@login_required
+def user_email_change():
+    form = EmailForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            try:
+                user_check = User.query.filter_by(email=form.email.data).first()
+                if user_check is None:
+                    user = current_user
+                    user.email = form.email.data
+                    user.email_confirmed = False
+                    user.email_confirmed_on = None
+                    user.email_confirmation_sent_on = datetime.now()
+                    db.session.add(user)
+                    db.session.commit()
+                    send_confirmation_email(user.email)
+                    flash('Email changed!  Please confirm your new email address (link sent to new email).', 'success')
+                    return redirect(url_for('user.user_profile'))
+                else:
+                    flash('Sorry, that email already exists!', 'error')
+            except IntegrityError:
+                flash('Error! That email already exists!', 'error')
+    return render_template('email_change.html', form=form)
+				
+
