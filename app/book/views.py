05543688f0db4d939fc473e0a214d0e6fde513c7
szapata1014 <szapata1@stevens.edu>
2016-12-01 19:24:31 -0500

    Added book details page

--- app/book/views.py
+++ app/book/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint, request, url_for, flash, redirect
 from flask_login import login_required, current_user
-from app.models import Book
+from app.models import Book, User
 from .forms import AddBookForm
 from app import db
 
@@ -38,3 +38,19 @@ def add_book():
 			flash('ERROR! Book was not added.', 'error')
 
 	return render_template('add_book.html', form=form)
+
+
+@book_blueprint.route('/book/<book_id>')
+def book_details(book_id):
+    book_with_user = db.session.query(Book, User).join(User).filter(Book.id == book_id).first()
+    if book_with_user is not None:
+        if book_with_user.Book.is_public:
+            return render_template('book_details.html', book=book_with_user)
+        else:
+            if current_user.is_authenticated and book_with_user.Book.user_id == current_user.id:
+                return render_template('book_detail.html', book=book_with_user)
+            else:
+                flash('Error! Incorrect permissions to access this book.', 'error')
+    else:
+        flash('Error! Book does not exist.', 'error')
+    return redirect(url_for('book.public_books'))
